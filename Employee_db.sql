7193ccf072b8f7db380e874ff6d0074ab60de63c
Peter Anderson <peter.g.anderson1@gmail.com>
2021-05-19 23:59:49 -0500

    started Analysis

---
+++ Employee_db.sql
@@ -0,0 +1,105 @@
+﻿DROP TABLE Employees;
+DROP TABLE Departments;
+DROP TABLE Depatment_Employees;
+DROP TABLE Department_Manager;
+DROP TABLE Salaries;
+DROP TABLE Titles;
+
+--Data Engineering - Create table for each csv file
+
+CREATE TABLE Employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Department_Employees (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR   NOT NULL
+);
+
+CREATE TABLE Department_Manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no INT   NOT NULL
+);
+
+CREATE TABLE Salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL
+);
+
+CREATE TABLE Titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL
+);
+
+ALTER TABLE Department_Employees ADD CONSTRAINT fk_Department_Employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Department_Employees ADD CONSTRAINT fk_Department_Employees_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Manager ADD CONSTRAINT fk_Department_Manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Manager ADD CONSTRAINT fk_Department_Manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+-- Checking table data was imported
+SELECT * from Employees;
+SELECT * from Departments;
+SELECT * from Department_Employees;
+SELECT * from Department_Manager;
+SELECT * from Salaries;
+SELECT * from Titles;
+
+-- Data Analysis
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.sex, Salaries.salary
+FROM Employees
+INNER JOIN Salaries on
+Employees.emp_no = Salaries.emp_no;
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT emp_no, first_name, last_name, hire_date from Employees
+WHERE hire_date BETWEEN '1985-12-31' AND '1987-01-01';
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT Department_Manager.dept_no,
+			Departments.dept_name,
+			Department_Manager.emp_no,
+			Employees.last_name,
+			Employees.first_name
+FROM Department_Manager
+INNER JOIN Departments on
+Department_Manager.dept_no = Departments.dept_no
+INNER JOIN Employees ON
+Department_Manager.emp_no = Employees.emp_no;
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
